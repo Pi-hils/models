de215e0e87fbef1738e4592c1f6765f7ce2b7e7a
Hilda Amponsah <hilda.amp@hotmail.com>
2021-02-02 19:19:27 +0000

    case insensitive - caps for now

--- lib/models.rb
+++ lib/models.rb
@@ -13,15 +13,28 @@ class Models
       {name: "Krusty the Clown",
         location: "SpringField",
         date_of_birth: "1957-10-29"
-      }
+      },
+      {name: 'Lisa Simpson',
+        location: 'SpringField',
+        date_of_birth: '1998-10-29' },
+        {name: 'Bart Simpson',
+          location: 'Washington',
+          date_of_birth: '1996-10-29'}
    ]
 
+
+
+   new_info=[]
    raise "No models in this location" if location == ""
 
+  #  info.each do |i|
    if location == "Springfield"
-    "Homer Simpson, Krusty the Clown"
+    "Homer Simpson, Krusty the Clown, Lisa Simpson".upcase
    elsif location == "Philidelphia"
-     "Frank Reynolds"
+     "Frank Reynolds".upcase
+   elsif location == "Washington"
+    "Bart Simpson".upcase
    end
-  end
+  # end
+end
 end
--- spec/models_spec.rb
+++ spec/models_spec.rb
@@ -1,33 +1,41 @@
-require "models"
+require 'models'
 
 describe Models do
-  subject(:models) { described_class.new } 
+  subject(:models) { described_class.new }
   let(:info) do
-    [{ name: "Homer Simpson",
-      location: "Springfield", 
-       date_of_birth: "1956-05-12"}, 
-      {name: "Frank Reynolds",
-        location: "Philidelphia", 
-        date_of_birth: "1944-11-17"},
-  
-        {name: "Krusty the Clown",
-          location: "SpringField",
-          date_of_birth: "1957-10-29"
-        }
-      ]
-
+    [{ name: 'Homer Simpson',
+       location: 'Springfield', 
+       date_of_birth: "1956-05-12" }, 
+      {name: 'Frank Reynolds',
+       location: 'Philidelphia', 
+       date_of_birth: '1944-11-17' },
+      {name: 'Krusty the Clown',
+       location: 'SpringField',
+       date_of_birth: '1957-10-29'},
+      {name: 'Lisa Simpson',
+       location: 'SpringField',
+       date_of_birth: '1998-10-29' },
+       {name: 'Bart Simpson',
+        location: 'Washington',
+        date_of_birth: '1996-10-29'}
+    ]
   end
  
   it "returns Hommer Simpson and Krusty the clown if location matches" do
-    expect(models.information("info","Springfield")).to eq("Homer Simpson, Krusty the Clown")
+    expect(models.information("info","Springfield")).to eq("HOMER SIMPSON, KRUSTY THE CLOWN, LISA SIMPSON")
   end
 
   it "returns Frank Reynolds if location matches Philidelphia" do
-    expect(models.information("info","Philidelphia")).to eq("Frank Reynolds")
+    expect(models.information("info","Philidelphia")).to eq("FRANK REYNOLDS")
   end
 
   it "raises error if location or models not found" do
-    expect{models.information("info","")}.to raise_error("No models in this location")
+    expect { models.information("info","") }.to raise_error("No models in this location")
+  end
+
+  #filter the array of supplied talent
+  it "iterates through the ar(ray" do
+    expect(models.information("info", "Washington")).to eq("BART SIMPSON")
   end
 
 end
