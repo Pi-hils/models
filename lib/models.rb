7a327311b1872ac29c0c23411fb41a1aa6acc72c
Hilda Amponsah <hilda.amp@hotmail.com>
2021-02-02 16:30:25 +0000

    Green satge - method created to accept 2 arguements -model info +location

---
+++ lib/models.rb
@@ -0,0 +1,5 @@
+class Models
+  def information(info,location)
+   info = [{:date_of_birth=>"1956-05-12", :location=>"Springfield", :name=>"Homer Simpson"}]
+  end
+end
--- spec/models_spec.rb
+++ spec/models_spec.rb
@@ -1,7 +1,8 @@
+require "models"
+
 describe Models do
   
-  it "has data of a model" do
-    expect(subject.information(info, place)).to eq [{"name": "Homer Simpson","location": "Springfield", "date_of_birth": "1956-05-12"}]
-
+  it "given data of a model" do
+    expect(subject.information("info", "location")).to eq [{"name": "Homer Simpson","location": "Springfield", "date_of_birth": "1956-05-12"}]
   end
 end
