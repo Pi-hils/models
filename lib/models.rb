b65d8eaeda29075427f0b9a9d3305fdeb9e3718f
Hilda Amponsah <hilda.amp@hotmail.com>
2021-02-02 16:34:09 +0000

    variable for the array of hashes

--- lib/models.rb
+++ lib/models.rb
@@ -1,5 +1,5 @@
 class Models
   def information(info,location)
-   info = [{:date_of_birth=>"1956-05-12", :location=>"Springfield", :name=>"Homer Simpson"}]
+   info = [{:date_of_birth=>"1956-05-12", :location=>"Springfield", :name=>"Homer Simpson"}, {"name": "Frank Reynolds","location": "Philidelphia", "date_of_birth": "1944-11-17"}]
   end
 end
--- spec/models_spec.rb
+++ spec/models_spec.rb
@@ -3,6 +3,7 @@ require "models"
 describe Models do
   
   it "given data of a model" do
-    expect(subject.information("info", "location")).to eq [{"name": "Homer Simpson","location": "Springfield", "date_of_birth": "1956-05-12"}]
+    info = [{"name": "Homer Simpson","location": "Springfield", "date_of_birth": "1956-05-12"},{"name": "Frank Reynolds","location": "Philidelphia", "date_of_birth": "1944-11-17"}]
+    expect(subject.information("info", "location")).to eq info
   end
 end
